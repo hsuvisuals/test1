5e84e7b75b614c048b0b4853ae5847615b830268
Jonathan Hsu <jonathanchun.hsu@sjsu.edu>
2018-11-10 20:49:48 -0800

    adding all the code

---
+++ img/bell_files/BizibleAcct.js
@@ -0,0 +1,12 @@
+(function () {
+    BizTrackingA.GoAccount({
+        webToLeadField: '',
+        chatEnabled: 'true' === 'true',
+        XUserId: 'aa2dc51da6274e66d991bd96eb684515',
+        formProviderEnabled: 'true' === 'true',
+        attach_secure_forms: 'true' === 'true',
+        viewThroughEnabled: 'true' === 'true',
+        interceptAjax: 'true' === 'true'
+    });
+})();
+;
